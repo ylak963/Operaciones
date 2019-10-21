9b82fe1ffed978c83733732feed550fe11e826ad
ylak963 <pepitogrilloreturn@gmail.com>
2019-10-21 11:42:53 +0200

    Version 0.0 Operaciones simples de aritmetica

---
+++ Operaciones1/src/es/studium/Operaciones1/Operaciones1.java
@@ -0,0 +1,32 @@
+package es.studium.Operaciones1;
+
+import java.util.Scanner;
+
+public class Operaciones1 {
+	public static void main(String[] args) {
+		int num1,num2;
+		//int suma, resta, producto;
+		//double cociente;
+		
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Escribe el primer número:");
+		num1=teclado.nextInt();
+		
+		System.out.println("Escribe el segun número:");
+		num2=teclado.nextInt();
+		
+		System.out.println("La suma de los dos números es:"+(num1+num2)+","+" "+
+				 "La recta de los dos numeros es:" +(num1-num2)+","+" "+ "El producto de los dos números es:"+(num1*num2)+
+				","+" "+ "El cociente de los dos números es:"+(num1/num2)+".");
+		
+		teclado.close();
+		if(num2!=0) {
+			System.out.println("El cociente vale"+ (num1/num2));
+			
+		}
+		else {
+			System.out.println("El numero no puede ser 0");
+		}
+	}
+
+}
